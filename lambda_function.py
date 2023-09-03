0dc0f6df696ea2e60525107169422b9a743ad9d7
sanghoon <shinsanghooon@gmail.com>
2023-09-03 19:37:14 +0900

    이미지에서 exif 데이터 추출 후 DynamoDB에 저장 기능 구현
    - 사진 생성 날짜, GPS 정보 저장

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,28 @@
 import json
 from PIL import Image
+from PIL.ExifTags import TAGS, GPSTAGS
 import boto3
 import os
 import logging
 
+def dms_to_decimal(gpsDMS, direction):
+    degrees = int(gpsDMS[0]) 
+    minutes = int(gpsDMS[1])
+    seconds = float(gpsDMS[2])
+
+    direction = direction.lower()
+    multiplier = 1 if direction in ['n', 'e'] else -1
+
+    decimal_degrees = degrees + (minutes / 60) + (seconds / 3600)
+    decimal_degrees *= multiplier
+
+    return decimal_degrees
+
+
 def lambda_handler(event, context):
+    
+    print(event);
+    
     logger = logging.getLogger(context.aws_request_id)
     logger.setLevel(logging.INFO)
 
@@ -61,14 +79,82 @@ def lambda_handler(event, context):
         logger.info(f'Start - Remove {target_key}')    
         os.remove(target_image_path)
         logger.info(f'End - Remove  {target_key}')
+
+    logger.info('Finish to extract thumbnail')
     
+    
+    # ----------------------------------------
+    # put image's exif info to dynamoDB
+
+    logger.info('Start to extract exif info')
+    baby_id = None
+    key_split = key.split('/')
+    if(key_split[0] == 'baby'):
+        baby_id = key_split[1]
+
+    exif_info = image._getexif()
+
+    exif = {}
+    for tag, value in exif_info.items():
+        if (tag not in ['MakerNote', 'PrintImageMatching']):
+            decoded = TAGS.get(tag, tag)
+            exif[decoded] = value
+
+    del exif['MakerNote']
+    del exif['PrintImageMatching']
+
+    
+    original_datetime = None 
+    if 'DateTime' in exif.keys():
+        original_datetime = exif['DateTime']
+
+    gps = {}
+    gps_latitude = None 
+    gps_longitude = None 
+    if 'GPSInfo' in exif.keys():
+        logger.info("GPS Info exists")
+        for key in exif['GPSInfo'].keys():
+            decode = GPSTAGS.get(key,key)
+            gps[decode] = exif['GPSInfo'][key]
+
+        gps_latitude = dms_to_decimal(gps["GPSLatitude"], gps["GPSLatitudeRef"])
+        gps_longitude = dms_to_decimal(gps["GPSLongitude"], gps["GPSLongitudeRef"]) 
+    else: 
+        logger.info("GPS Info doesn't exist")
+
+    dynamodb = boto3.client("dynamodb")
+
+    item = {
+            'baby_id': {'S': str(baby_id)},  
+            'key_name': {'S': key},
+            'original_created_at': {'S': str(original_datetime)}, 
+            'gps_latitude': {'S': str(gps_latitude)} ,
+            'gps_longitude': {'S': str(gps_longitude)},
+            'gps_info': {'S': str(gps)}
+        }
+
+    table_name = 'image_exif_info_dev' 
+
+    logger.info(f'Start - put data to dynamoDB({table_name})')
+    try:
+        # DynamoDB에 데이터 쓰기
+        response = dynamodb.put_item(
+            TableName=table_name,
+            Item=item
+        )
+        logger.info(f"successs put data to dynabodb : {response}")
+    except Exception as e:
+        logger.info(f"fail put data to dynabodb: {str(e)}")
+        raise e;
+    logger.info(f'End - put data to dynamoDB({table_name})')
+
+    logger.info('Finish to extract exif info')
+
     # clear tmp directory and file
     logger.info(f'Remove Tmp source image')
     os.remove(source_image_path)
-    
-    logger.info('Finish to extract thumbnail')
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Image processing completed!')
+        'body': json.dumps('Image processing and Extract exif info completed!')
     }
--- local_extract_thumbnails.py
+++ local_extract_thumbnails.py
@@ -1,8 +1,73 @@
-from PIL import Image
-from deepface import Deepface
+from PIL import Image, ExifTags
+from PIL.ExifTags import TAGS, GPSTAGS
 import os
+import exif 
 
-image = Image.open("samples/6.jpg")
+
+# ------------------------------------------------
+# put image exif data to dynamodbDB
+
+event_key = "baby/3/profile/DSCF7000.JPG";
+
+baby_id = None
+key_split = event_key.split('/')
+if(key_split[0] == 'baby'):
+	baby_id = key_split[1]
+
+image = Image.open("samples/9.jpg")
+
+exif_info = image._getexif()
+
+exif = {}
+for tag, value in exif_info.items():
+    decoded = TAGS.get(tag, tag)
+    exif[decoded] = value
+del exif['MakerNote']
+
+original_datetime = None 
+if 'DateTime' in exif.keys():
+	original_datetime = exif['DateTime']
+
+gps = {}
+gps_latitude = None 
+gps_logitude = None 
+if 'GPSInfo' in exif.keys():
+	print("Yes GPS Info exists")
+	for key in exif['GPSInfo'].keys():
+	    decode = GPSTAGS.get(key,key)
+	    gps[decode] = exif['GPSInfo'][key]
+
+	gps_latitude = dms_to_decimal(gps["GPSLatitude"], gps["GPSLatitudeRef"])
+	gps_longitude = dms_to_decimal(gps["GPSLongitude"], gps["GPSLongitudeRef"]) 
+else: 
+	print("GPS Info doesn't exist")
+
+dynamodb = boto3.client("dynamodb")
+
+item = {
+        'baby_id': {'S': str(baby_id)},  
+        'original_created_at': {'S': str(original_datetime)}, 
+        'gps_logitude': {'S': str(gps_latitude)} ,
+        'gps_latitude': {'S': str(gps_longitude)},
+        'gps_info': {'S': str(gps)},
+        'exif_raw': {'S', str(exif)},
+    }
+
+table_name = 'image_exif_info_dev' 
+
+try:
+    # DynamoDB에 데이터 쓰기
+    response = dynamodb.put_item(
+        TableName=table_name,
+        Item=item
+    )
+    print(f"데이터 쓰기 성공: {response}")
+except Exception as e:
+    print(f"데이터 쓰기 실패: {str(e)}")
+
+
+# ----------------------------------------------------
+# put image to s3 after crop and resize 
 
 sourceHeight = image.height
 sourceWidth = image.width
@@ -38,3 +103,19 @@ def printSize(image):
 	print(f'Height: {image.height}, Width: {image.width}')
 
 
+def dms_to_decimal(gpsDMS, direction):
+
+    # 문자열로 입력된 도, 분, 초를 정수로 변환
+    degrees = int(gpsDMS[0]) 
+    minutes = int(gpsDMS[1])
+    seconds = float(gpsDMS[2])
+
+    # 방향 문자열을 소문자로 변환하여 N과 E인 경우 양수로, S와 W인 경우 음수로 설정
+    direction = direction.lower()
+    multiplier = 1 if direction in ['n', 'e'] else -1
+
+    # 십진수 좌표 계산
+    decimal_degrees = degrees + (minutes / 60) + (seconds / 3600)
+    decimal_degrees *= multiplier
+
+    return decimal_degrees
